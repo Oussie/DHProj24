0ec6fe24e73aa498e89bc1a7ea48abd7ca75e4d1
Oscar Lihou-Smith <olihousmith@gmail.com>
2024-11-02 17:11:57 +0000

    render functionality with random colours as placeholder

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import numpy as np
+from src.render import render
 
-# Set the dimensions of the grid
-rows, cols = 10, 10
-# Generate a 64x64 grid with random integers from 1 to 5
-grid = np.random.randint(1, 6, size=(rows, cols))
-# Print the grid
-print(grid)
-
-my_dict = {1: None, 2: None, 3: None, 4: None, 5: None}
+render()
