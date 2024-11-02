b795cf02ac0b63af4d10d80f5f9f7a15e2da2293
Oscar Lihou-Smith <olihousmith@gmail.com>
2024-11-02 19:42:12 +0000

    link asset manager and renderer

--- src/assets.py
+++ src/assets.py
@@ -4,7 +4,7 @@ class AssetManager:
 
     def __init__(self):
         pygame.init()
-        self.surface = pygame.image.load('../assets/tileset.png').convert()
+        self.surface = pygame.image.load('/Users/oscarlihou-smith/Desktop/durhack/DHProj24/assets/tileset.png').convert()
     
     def get(self, x, y):
         square = pygame.Rect(x, y, 16, 16)
