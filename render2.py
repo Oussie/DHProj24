67ceb220161959418a7ca79daac3cd346c44e8bd
Johnny Ho <jdho2006@gmail.com>
2024-11-03 01:10:15 +0000

    Added generation to rendering

--- render2.py
+++ render2.py
@@ -4,12 +4,13 @@ import os
 from assets import AssetManager
 from random import randint
 from tiles import TileManager
+from generation import generate
 
 # Initialize Pygame
 pygame.init()
 
 # Set up display
-GRID_SIZE = 64       # 64x64 grid
+GRID_SIZE = 40       # 64x64 grid
 TILE_SIZE = 16       # Each image tile is 16x16 pixels
 screen_width = GRID_SIZE * TILE_SIZE
 screen_height = GRID_SIZE * TILE_SIZE
@@ -18,7 +19,7 @@ pygame.display.set_caption("Grid of Images")
 AssetManager.load()
 screen.fill((0, 0, 0))
 
-arr = [[randint(1,24) for i in range(80)] for j in range(80)]
+arr = generate((GRID_SIZE, GRID_SIZE))
 
 # Main loop
 running = True
@@ -32,8 +33,6 @@ while running:
         for x,id in enumerate(row):
             index = TileManager.get(id)["index"]
             tile = AssetManager.get(*index)
-
-            print(index)
 
             # Blit the image at the calculated position
             screen.blit(tile, (x*TILE_SIZE, y*TILE_SIZE))
